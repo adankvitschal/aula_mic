3899e521bf7e853305197b3314ec48e974c25ec3
Adan Kvitschal <adan.kvitschal@ifsc.edu.br>
2025-02-04 09:30:07 -0300

    primeiro commit, esqueleto

---
+++ aula_mic/main.c
@@ -0,0 +1,16 @@
+/*
+ * main.c
+ *
+ * Created: 2/4/2025 9:16:50 AM
+ *  Author: Aluno
+ */ 
+
+#include <xc.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
